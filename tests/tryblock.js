57007a5223970f168e21245942a9f1ea7f2b80f3
Alexander Parsan <alexanderparsan28@gmail.com>
2015-04-25 15:33:01 -0400

    Removed Sorting Temporarily, No Longer Requires Two DB for Server Checking

---
+++ tests/tryblock.js
@@ -0,0 +1,14 @@
+try {
+    adddlert("Welcome guest!");
+}
+catch(err) {
+    console.log(err.message);
+
+
+
+}
+
+console.log("Just keep on going...")
+var test = true;
+var test2 = "keep on rolling";
+console.log(test2)
