592e636122d8c75540076e4ee662c9997183216a
Alexander Parsan <alexanderparsan28@gmail.com>
2015-05-09 10:14:52 -0400

    Created space for 100 Delays at once

---
+++ generateid.py
@@ -0,0 +1,13 @@
+i = 0
+while i < 100:
+	string = """"key_""" + str(i) + """  " """ + ":"
+	newstring = string.replace(" ", "")
+
+	comma = " ,"
+	comma = comma.replace(" ", "")
+
+
+	print(newstring),
+	print(str(i)),
+	print("" + comma) 
+	i=i+1
